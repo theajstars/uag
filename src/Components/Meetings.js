a86cec5ac1f6dbb57356b77e0b1af0f7e7522b59
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2022-08-21 22:45:07 +0100

    Added calendar datepicker component

--- src/Components/Meetings.js
+++ src/Components/Meetings.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import meetings from "../Assets/JSON/Meetings.json";
+import { Modal, Input, Calendar } from "antd";
 
 export default function Meetings() {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [myMeetings, setMyMeetings] = useState(meetings.meetings);
+  const onPanelChange = (value, mode) => {
+    // console.log(value.format("YYYY-MM-DD"), mode);
+  };
+  const onMeetingDateChange = (value) => {
+    console.log(value.format("YYYY-MM-DD"));
+  };
   return (
     <div className="card-default card-padding meeting-card flex-column">
       <div className="flex-row card-header-row">
@@ -11,25 +21,59 @@ export default function Meetings() {
         </span>
       </div>
       <hr className="hr-default" />
-      <div className="meeting flex-row">
-        <div className="card-text card-text-large">Financial Plan Meeting</div>
-        <div className="meeting-details flex-column">
-          <span className="card-text card-text-small">30 June 2022</span>
-          <span className="card-text card-text-smaller text-red">
-            09:00 - 10:30
-          </span>
-        </div>
+      {myMeetings.map((meeting, index) => {
+        return (
+          <div className="meeting flex-row">
+            <div className="card-text card-text-large">{meeting.title}</div>
+            <div className="meeting-details flex-column">
+              <span className="card-text card-text-small">{meeting.date}</span>
+              <span className="card-text card-text-smaller text-red">
+                {meeting.time}
+              </span>
+            </div>
+          </div>
+        );
+      })}
+      <Modal
+        title="My Meetings"
+        visible={isModalVisible}
+        onOk={() => setModalVisible(false)}
+        onCancel={() => setModalVisible(false)}
+      >
+        {myMeetings.map((meeting, index) => {
+          return (
+            <div className="my-meetings-container">
+              <div className="meeting flex-row">
+                <div className="card-text card-text-large">{meeting.title}</div>
+                <div className="meeting-details flex-column">
+                  <span className="card-text card-text-small">
+                    {meeting.date}
+                  </span>
+                  <span className="card-text card-text-smaller text-red">
+                    {meeting.time}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        <br />
+        <h4>New Meeting</h4>
+        <Input placeholder="Meeting Title" />
+        <br />
+        <br />
+        <h4>Meeting Date</h4>
+        <Calendar
+          onPanelChange={onPanelChange}
+          onChange={onMeetingDateChange}
+        />
+      </Modal>
+      <div className="more-button-container">
+        <button className="more-button" onClick={() => setModalVisible(true)}>
+          More
+        </button>
       </div>
-      <div className="meeting flex-row">
-        <div className="card-text card-text-large">Stakeholder Engagement</div>
-        <div className="meeting-details flex-column">
-          <span className="card-text card-text-small">2 July 2022</span>
-          <span className="card-text card-text-smaller text-red">
-            10:00 - 11:30
-          </span>
-        </div>
-      </div>
-      <MoreButton />
+      {/* <MoreButton event={setModalVisible} start={isModalVisible} /> */}
     </div>
   );
 }
